371e6a8d11180691e9e57b24a14d5a32b365fc23
sho0126hiro <sho0126hiro@yahoo.co.jp>
2017-05-27 22:41:41 +0900

    真偽判定

---
+++ if_test.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/*
+
+^‹U”»’è
+
+if(x)printf("a");
+if(x==1)printf("a");
+‚Í“¯‚¶
+
+if(x)printf("a");
+if(x<=1)printf("a");
+‚Í“¯‚¶
+
+if(!x)printf("b");
+of(x==0)prinf("b");
+‚Í“¯‚¶
+
+*/
+
+int main(){
+	int x=0,y=2,z=1;
+	int x2=0,y2=2,z2=1;
+	
+	if(x==1)printf("x\n");
+	if(y==0)printf("y\n");
+	if(z<=1)printf("z\n");
+	
+	if(x2)printf("xx\n");
+	if(!y2)printf("yy\n");
+	if(z2)printf("zz\n");
+	
+	return 0;
+}
